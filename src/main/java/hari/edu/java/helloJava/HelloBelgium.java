2c7eefab01f3034807112ab4892626590618b456
HariHaraKumar.C <hariharakumar.c@outlook.com>
2017-06-04 21:26:56 +0200

    Initial Commit

---
+++ src/main/java/hari/edu/java/helloJava/HelloBelgium.java
@@ -0,0 +1,11 @@
+package hari.edu.java.helloJava;
+
+/**
+ * Created by HariHaraKumar.C on 6/1/2017.
+ */
+public class HelloBelgium {
+
+    public static void main(String args[]){
+        System.out.println("Hello Belgium");
+    }
+}
