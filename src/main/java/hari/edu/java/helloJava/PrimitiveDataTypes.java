fa5e2c548e4afdedaac4bad16c64660e03f08f66
HariHaraKumar.C <hariharakumar.c@outlook.com>
2017-06-06 20:34:02 +0200

    Java Primitive Data Types

---
+++ src/main/java/hari/edu/java/helloJava/PrimitiveDataTypes.java
@@ -0,0 +1,31 @@
+package hari.edu.java.helloJava;
+
+/**
+ * Created by HariHaraKumar.C on 6/5/2017.
+ * Description: Java Primitive Data Types
+ */
+public class PrimitiveDataTypes {
+
+    public static void main(String args[]){
+        //Byte: 8 bits (-127 to 127)
+        byte byteVariable;
+        byteVariable=127;
+        System.out.println("Primitive data type -> BYTE :  "+ byteVariable);
+        //Short: 16 bits (-32768 to 32767)
+        short shortVariable;
+        shortVariable=32767;
+        System.out.println("Primitive data type -> SHORT :  "+ shortVariable);
+        //Int: 32 bits
+        int intVariable;
+        intVariable=2;
+        System.out.println("Primitive data type -> INT :  "+ intVariable);
+        //Long: 64 Bit
+        //Float: 32 bit (Floating point types)
+        //Double: 64 Bit (Floating point types)
+        //Boolean: True or False
+        //Char: 16 Bit (Contains Unicode Characters)
+        char charVariable;
+        charVariable='A';
+        System.out.println("Primitive data type -> CHAR :  "+ charVariable);
+    }
+}
